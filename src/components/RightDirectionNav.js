f1a529f225fe01d7e62affd3c141cec4f88bea92
Sona <sona30e@gmail.com>
2021-05-07 13:13:22 -0700

    github pages

--- src/components/RightDirectionNav.js
+++ src/components/RightDirectionNav.js
@@ -41,12 +41,11 @@ export default function RightDirectionNav({ open }) {
     // document.querySelector(".sc-bdnxRM").classList.add("gEseQU")
     // document.querySelector(".sc-bdnxRM").classList.remove("fuRgLU")
     // document.querySelector(".sc-gtsrHT").classList.add("fXvcEa")
-    document.querySelector(".sc-gtsrHT").classList.remove("fzGQbW")
-    document.querySelector(".sc-bdnxRM").classList.add("ctYbLy")
-    document.querySelector(".sc-gtsrHT").classList.remove("ctYbLy")
-    document.querySelector(".sc-bdnxRM").classList.remove("jmvFTq")
-    document.querySelector(".sc-gtsrHT").classList.add("gAuIUi")
-    document.querySelector(".sc-bdnxRM").classList.remove("gAuIUi")
+    document.querySelector(".sc-gtsrHT").classList.add("fzGQbW")
+    document.querySelector(".sc-bdnxRM").classList.remove("ctYbLy")
+  
+    document.querySelector(".sc-bdnxRM").classList.add("jmvFTq")
+    document.querySelector(".sc-gtsrHT").classList.remove("gAuIUi")
 
   
   };
